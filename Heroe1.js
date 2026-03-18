d5ac1942f15f58ae1e75fc8c7f48b34c723cbb4b
dapdneh <dapdneh@gmail.com>
2026-03-18 09:15:19 -0500

    Create Heroe1.js

---
+++ Heroe1.js
@@ -0,0 +1,16 @@
+fead:agregar ficha y narrativa de mi suoerheroe 
+//creacion de variables 
+let nombreHeroe = "hulk"; 
+let nivelEnergia = 80;
+let ciudad = "Paris";
+let enemigo = "catnoir"
+
+//mostrar en la pantalla
+console.log("---inicio de la aventura---");
+console.log(nombreHeroe + " esta defendiendo la ciudad de " + ciudad);
+console.log(" su energia actual es de " + nivelEnergia)
+
+//cambiar el valor
+nivelEnergia = 75; //el heroe se canso 
+console.log("---noticia de ultimo minuto---");
+console.log(nombreHeroe + " peleó contra " + enemigo + " y ahora su energia es " + nivelEnergia)
